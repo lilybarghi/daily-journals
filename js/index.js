dd9c1f16b4e15635c33ff9f07963c6ba7fded1be
Kelly Wachtel <kwachtel24@gmail.com>
2019-03-04 18:46:37 -0600

    bug fixes and add entry button

--- js/index.js
+++ js/index.js
@@ -1,37 +1,11 @@
+let thePresent = new Date();
+let currMonth = thePresent.getMonth();
+let currYear = thePresent.getFullYear();
+let months = ["January ", "February ", "March ", "April ", "May ", "June ", "July ", "August ", "September ", "October ", "November ", "December "]
+
 $(document).ready( function() { 
-    journals = window.app.journals;
-    thePresent = new Date();
-    currMonth = thePresent.getMonth();
-    currYear = thePresent.getFullYear();
-    months = ["January ", "February ", "March ", "April ", "May ", "June ", "July ", "August ", "September ", "October ", "November ", "December "]
-
+    let journals = window.app.journals;
     handle_calendar(currMonth, currYear);
-
-    $(".day").click(function() {
-        let modal = $("#myModal");
-        modal.css("display", "block");
-        let day = this.innerHTML;
-        day = day.substr(25);
-        let entry = journals.find((journal) => {
-            let journalDate = new Date(journal.date);
-            return (journalDate.getMonth() == currMonth) && (journalDate.getDate() == day) && (journalDate.getFullYear() == currYear);
-        })
-        console.log(entry);
-        if(entry === undefined) {
-            let modal = $("#myModal");
-            modal.css("display", "none");
-            return;
-        }
-        $("#header-date").html(months[currMonth] + " " + day + ", " + currYear);
-        $("#entry-title").html("<strong>" + entry.title + "</strong>");
-        let emotions = entry.emotions.join(', ');
-        console.log(emotions);
-        $("#list-emotions").html(emotions);
-        $("#entry-goal").html(entry.goal);
-        $("#entry-text").html(entry.entry);
-
-        
-    });
 
     $(".close").click(function() {
         let modal = $("#myModal");
@@ -74,6 +48,7 @@ function handle_next() {
 }
 
 function handle_calendar(month, year) {
+    let journals = window.app.journals;
     
     //disply the month and year above calendar
     header = document.getElementById("month-name");
@@ -160,8 +135,10 @@ function handle_calendar(month, year) {
                 }
                 if ((date === thePresent.getDate()) && (year === thePresent.getFullYear()) && (month === thePresent.getMonth() && (todaysJournal !== undefined))){
                     day.setAttribute("class", "active day has-journal"); //set ID for today's date so it can be marked with CSS
+                    day.setAttribute("onclick", "showEntry(this)");
                 } else if ((todaysJournal !== undefined)){
                     day.setAttribute("class", "day has-journal");
+                    day.setAttribute("onclick", "showEntry(this)");
                 } else if((date === thePresent.getDate()) && (year === thePresent.getFullYear()) && (month === thePresent.getMonth())) {
                     day.setAttribute("class", "active day");
                 } else {
@@ -189,6 +166,32 @@ function handle_calendar(month, year) {
         }
         cal.appendChild(row);
     }
+}
+
+function showEntry(dayObj) {
+    let journals = window.app.journals;
+    let modal = $("#myModal");
+    modal.css("display", "block");
+    let day = dayObj.innerHTML;
+    day = day.substr(25);
+    console.log(day);
+    let entry = journals.find((journal) => {
+        let journalDate = new Date(journal.date);
+        return (journalDate.getMonth() == currMonth) && (journalDate.getDate() == day) && (journalDate.getFullYear() == currYear);
+    })
+    console.log(entry);
+    if(entry === undefined) {
+        let modal = $("#myModal");
+        modal.css("display", "none");
+        return;
+    }
+    $("#header-date").html(months[currMonth] + " " + day + ", " + currYear);
+    $("#entry-title").html("<strong>" + entry.title + "</strong>");
+    let emotions = entry.emotions.join(', ');
+    console.log(emotions);
+    $("#list-emotions").html(emotions);
+    $("#entry-goal").html(entry.goal);
+    $("#entry-text").html(entry.entry);
 }
 
 function validateForm() {
